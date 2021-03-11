51cb5b5ba2c9d9bf5b312d767f26f85bf6f5f1bd
abay-2002 <akbar121202@gmail.com>
2021-03-11 17:59:27 +0700

    wohooo script menambahkan kegiatan sudah berjalan tinggal 2 script lagi, 2.Centang kegiatan 3.Hapus Kegiatan[C

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-var inputKegiatanUser = document.getElementById('inputKegiatan');
-var tombolEnter = document.getElementById('tombolOk');
+var tampilkanValue = document.getElementById('tombolOk');
+tampilkanValue.addEventListener('click',function(){
+	// select input element dan ambil valuenya
+	var tampungInput = document.getElementById('inputKegiatan').value;	
+	// bikin element li
+	const elhSection = document.createElement('section');
+	const elhUl = document.createElement('ul');
+	elhSection.setAttribute('id','sectionParent');
+	elhUl.setAttribute('id','parentKegiatan');
+	elhSection.appendChild(elhUl);
+	// output kode diatas: <section id='sectionParent'><ul></ul></section>
+	// kode diatas berupa membuat section
+	
+	const elhLi = document.createElement('li');
+	const elhSpan = document.createElement('span');
+	const elhImg = document.createElement('img');
+	elhImg.setAttribute('src','assets/img/removeicon.png');
+	elhSpan.innerHTML = tampungInput;
+	elhLi.setAttribute('class','aktivitas');
+	elhLi.appendChild(elhSpan);
+	elhLi.appendChild(elhImg);
+	// output kode diatas: <li class="aktivitas"><span>kegiatan</span><img src="assets/img/removeicon.png"></li>
+	// output kode diatas berupa membuat element li
+	elhUl.appendChild(elhLi);
+	// kode diatas saya memberi child li kepada parent ul
+
+	const sectionWrapper = document.getElementById('sectionWrapper');
+	sectionWrapper.appendChild(elhSection);
+	console.log(sectionWrapper);
+});
+
+
 
 
 // bikin li seperti ini <li class="aktivitas"><span>kegiatan</span><img src="assets/img/centang.png"></li>
+
+
+
+
+// elhSpan.innerHTML = tampungInput;
+
+
+
 
 
 // idenya adalah
